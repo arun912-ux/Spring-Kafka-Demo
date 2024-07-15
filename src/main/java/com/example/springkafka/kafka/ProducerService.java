f27c503dfc19382e7361614bb3b782fdcf5882ad
arun912-ux <arunshanagonda777@gmail.com>
2024-07-15 14:51:13 +0530

    consumer working..

--- src/main/java/com/example/springkafka/kafka/ProducerService.java
+++ src/main/java/com/example/springkafka/kafka/ProducerService.java
@@ -1,31 +1,37 @@
 package com.example.springkafka.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import net.datafaker.Faker;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private static final String OUTPUT_TOPIC = "test-topic";
     Faker faker = Faker.instance();
+
 
     public ProducerService(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    @Scheduled(cron = "*/10 * * * * *")
+
+//    @Scheduled(cron = "*/10 * * * * *")
+    @Scheduled(initialDelay = 3000, fixedDelay = 30_000)
     public void sendMessageWithDelay() {
         String message = faker.dune().quote();
-        System.out.println("sending message = " + message + "\n");
-        kafkaTemplate.send("test-topic", message);
+        log.info("sending message : {} \n", message);
+        kafkaTemplate.send(OUTPUT_TOPIC, message);
     }
 
     public void sendMessage() {
         String message = faker.ancient().god();
-        System.out.println("message = " + message);
-        kafkaTemplate.send("test-topic", message);
+        log.info("message : {} \n", message);
+        kafkaTemplate.send(OUTPUT_TOPIC, message);
     }
 
 }
